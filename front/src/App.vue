44c146b627fef8dc481e76785aa2d9de09020438
iuchi-2020 <iuchi.masatoshi@gmail.com>
2021-06-25 23:51:08 +0900

    update:入力フォームを追加

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="こんにちは" />
+  <input type="text" id="sampleText" name="sampleText" required />
 </template>
 
 <script>
